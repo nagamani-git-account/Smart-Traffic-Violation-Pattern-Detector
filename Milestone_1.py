32c35c1c6d1c80a7ef73fde0f658e4ccccd50020
nagamani-git-account <nagamanipuvvala33@gmail.com>
2025-10-25 16:16:46 +0530

    Second commit

--- Milestone_1.py
+++ Milestone_1.py
@@ -1,28 +1,11 @@
-from pyspark.sql.types import StructType, StructField, StringType, TimestampType, IntegerType, DoubleType
 import random
 import datetime
 import pandas as pd
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import to_timestamp
+from pyspark.sql.types import StructType, StructField, StringType, TimestampType, IntegerType, DoubleType
 
 spark = SparkSession.builder.appName("TrafficViolation").getOrCreate()
-
-violation_types = ["Speeding", "Red Light", "No Helmet", "Wrong Lane"]
-vehicle_types = ["Car", "Bike", "Bus", "Truck"]
-
-data = []
-for i in range(1000):  # 1000 sample records
-    data.append({
-        "Violation_ID": f"V{i+1:04d}",
-        "Timestamp": str(datetime.datetime.now() - datetime.timedelta(minutes=random.randint(0, 10000))),
-        "Location": f"Intersection_{random.randint(1, 20)}",
-        "Violation_Type": random.choice(violation_types),
-        "Vehicle_Type": random.choice(vehicle_types),
-        "Severity": random.randint(1, 5)
-    })
-
-df = pd.DataFrame(data)
-df.to_csv("traffic_data.csv", index=False)
 
 schema = StructType([
     StructField("Violation_ID", StringType(), True),
@@ -35,6 +18,7 @@ schema = StructType([
 
 violation_types = ["Speeding", "Red Light", "No Helmet", "Wrong Lane"]
 vehicle_types = ["Car", "Bike", "Bus", "Truck"]
+
 
 data = []
 for i in range(1000):  # 1000 sample records
@@ -61,3 +45,4 @@ valid_types = ["Speeding", "Red Light", "No Helmet", "Wrong Lane"]
 df_clean = df_clean.filter(df_clean.Violation_Type.isin(valid_types))
 
 df_clean.write.parquet("cleaned_traffic_data.parquet")
+
